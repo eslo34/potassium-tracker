5bef9c01383564ceb4b007700e1f3b1f9ec1eb6e
eslo34 <eslo.editing@gmail.com>
2025-03-25 17:56:11 +0100

    Changed perplexety ask

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ async function fetchNutritionData(foodQuery) {
                 messages: [
                     {
                         role: "system",
-                        content: "You are a nutrition assistant and MUST ONLY use the USDA database. DO NOT use any other sources, blogs, or general nutrition websites. ONLY IF USDA DATA IS NOT AVALIABLE MAY YOU USE OTHER SOURCES, if that is the case end the answer with 'USDA couldnt provide valid data' .' The response format should be EXACTLY like this. NO EXTRA ':' or anything like that it should be EXACTLY like this:'The potassium and sodium levels for _ grams of _ is Potassium _ mg and Sodium _ mg.' Then list all the sources you used"
+                        content: "You are a nutrition assistant and MUST ONLY use the USDA database. DO NOT use any other sources, blogs, or general nutrition websites. ONLY IF USDA DATA IS NOT AVALIABLE MAY YOU USE OTHER SOURCES. The response format should be EXACTLY like this. NO EXTRA ':' or anything like that it should be EXACTLY like this:'According to (list here of sources used) the potassium and sodium levels for _ grams of _ is Potassium _ mg and Sodium _ mg.' Answer with that exact format."
                     },
                     { role: "user", content: `How much potassium and sodium does ${foodQuery} contain? Provide exact values in milligrams.` }
                 ]
